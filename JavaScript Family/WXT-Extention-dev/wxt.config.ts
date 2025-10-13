ed62fd24448712d921cddffabb75e9b2d4553400
Shaurya Jain <103116023+ShamsForge@users.noreply.github.com>
2025-10-13 19:14:18 +0530

    code cleanup and assets

--- JavaScript Family/WXT-Extention-dev/wxt.config.ts
+++ JavaScript Family/WXT-Extention-dev/wxt.config.ts
@@ -1,16 +1,30 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: "src",             // default: "."
-  modules: ['@wxt-dev/module-react'],
-  manifest: {
+  srcDir: "src", // default: "."
+  modules: ["@wxt-dev/module-react"],
+  manifest: ({ browser, manifestVersion, mode, command }) => {
     return {
-    name: 'My Extension',
-    version: '1.0.0',
-    description: 'A description of my extension',
-    permissions: ['storage', 'tabs'],
-  }
-  }
-
+      name: "The Broken Script",
+      description: (
+        "This extention is a continuation of a Famous Minecraft Horror mod called The Broken Script."
+        + " This is a slow paced horror script that will try to make you feel paranoid. There are random events, few entities and anomalies (and much more)."
+        + ""
+        + " **USER ADVISORY**"
+        + " - It contains Disturbing Imagery and Sounds."
+        + " - Events can happen at any time, even when you are not looking at the browser."
+        + " - It can make your browser look glitched and broken."
+        + " - It can add scary sounds and Graphical Jumpscares to your browser."
+        + " - It can even add fake virus alerts to your browser."
+        + ""
+        + "This is a fanmade extension version of The Broken Script mod and it is Open-Source. and it is 100% safe and never share your data."
+        + ""
+        + " I dare you to try it out, if you are brave enough for this halloween 2025."
+      ),
+      version: "0.1.0",
+      manifest_version: 3,
+      // permissions: ['storage', 'tabs'],
+    };
+  },
 });
